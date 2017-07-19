04b51e07965069bed8044acc5dfa295d9c40919a
Toni Ruottu <toni.ruottu@iki.fi>
2017-07-19 14:09:45 +0300

    Deploy dwst 2.1.1

--- dwst.js
+++ dwst.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '2.1.0';
+const VERSION = '2.1.1';
 const ECHO_SERVER_URL = 'wss://echo.websocket.org/';
 const bins = new Map();
 const texts = new Map();
@@ -1051,7 +1051,6 @@ class Help {
         available.push(commandSegment);
       }
     });
-    available.sort();
 
     const commandsList = [flatList('Commands', available)];
 
@@ -1353,20 +1352,27 @@ function mlog(lines, type) {
           const textChunks = divissimo(rawText, 4);
 
           const byteGrid = document.createElement('div');
-          byteGrid.setAttribute('class', 'dwst-bytegrid');
+          const byteGridClasses = ['dwst-bytegrid'];
+          if (hexChunks.length < 3) {
+            byteGridClasses.push('dwst-bytegrid--less-than-three');
+          }
+          byteGrid.setAttribute('class', byteGridClasses.join(' '));
 
-          hexChunks.forEach((hexChunk, i) => {
-            const textChunk = textChunks[i];
+          const chunksWanted = 4;
+          const chunkLength = 4;
+          [...Array(chunksWanted).keys()].forEach(i => {
+            const [hexChunk = []] = [hexChunks[i]];
+            const [textChunk = []] = [textChunks[i]];
 
             const hexContent = htmlescape(hexChunk.join(' '));
             const hexItem = document.createElement('div');
-            hexItem.setAttribute('class', 'dwst-bytegrid_item');
+            hexItem.setAttribute('class', 'dwst-bytegrid__item');
             hexItem.innerHTML = hexContent;
             byteGrid.appendChild(hexItem);
 
-            const textContent = htmlescape(textChunk.join(''));
+            const textContent = htmlescape(textChunk.join('').padEnd(chunkLength));
             const textItem = document.createElement('div');
-            textItem.setAttribute('class', 'dwst-bytegrid_item');
+            textItem.setAttribute('class', 'dwst-bytegrid__item');
             textItem.innerHTML = textContent;
             byteGrid.appendChild(textItem);
           });
@@ -1432,6 +1438,7 @@ function gfx(lines, colors) {
 
   const gfxContainer = document.createElement('div');
   gfxContainer.setAttribute('class', 'dwst-gfx');
+  gfxContainer.setAttribute('aria-hidden', 'true');
   gfxContainer.appendChild(gfxContent);
 
   const terminal1 = document.getElementById('ter1');
@@ -1875,3 +1882,4 @@ function init() {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+window.addEventListener('load', updateGfxPositions);
