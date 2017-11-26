e067afe7bfb74a6938534e0a2fb2ad18813ce3b2
Toni Ruottu <toni.ruottu@iki.fi>
2017-11-26 15:24:56 +0200

    Deplot dwst 2.4.5

--- scripts/dwst.js
+++ scripts/dwst.js
@@ -758,7 +758,7 @@ var terminal = new _terminal2.default('ter1', controller);
 
 var pluginInterface = {
 
-  VERSION: '2.4.4',
+  VERSION: '2.4.5',
   ECHO_SERVER_URL: 'wss://echo.websocket.org/',
 
   terminal: terminal,
